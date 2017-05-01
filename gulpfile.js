402e0f40d47b0a9bab028c5ac73e6450268bc6a3
Alex Wang <allenallen2525@gmail.com>
2017-05-01 23:33:51 +0800

    header + side menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp           = require('gulp'),
-    // connect = require('gulp-connect'),
     webserver      = require('gulp-webserver'),
-    gulpCompass    = require('gulp-compass'),
-    gulpLivereload = require('gulp-livereload');  // 載入 gulp-livereload
+    gulpCompass    = require('gulp-compass');
 
 gulp.task('webserver', function() {
     gulp.src('./')
@@ -15,23 +13,19 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('compass', function () {
-    gulp.src('scss/*.scss') // sass 來源路徑
+    gulp.src('scss/*.scss')     // sass 來源路徑
         .pipe(gulpCompass({
-            css: 'css',             // compass 輸出位置
-            sass: 'scss',           // sass 來源路徑
-            image: 'img',           // 圖片來源路徑
+            css: 'css',         // compass 輸出位置
+            sass: 'scss',       // sass 來源路徑
+            image: 'img',       // 圖片來源路徑
             style: 'nested',    // CSS 處理方式，預設 nested（expanded, nested, compact, compressed）
-            comments: false,        // 是否要註解，預設(true)
-            require: ['susy'],      // 額外套件 susy
+            comments: false,    // 是否要註解，預設(true)
+            require: ['susy'],  // 額外套件 susy
         }));
-        // .pipe(gulpLivereload());
-        // .pipe(gulp.dest('app/assets/temp')); // 輸出位置(非必要)
 });
 
 gulp.task('watch',function(){
-    // gulpLivereload.listen();
     gulp.watch(['./scss/*.scss'],['compass']);
-    // gulp.watch(['./js/*.js', './css/*.css', './html/*.html', './index.html'],['reload']);
 });
 
 gulp.task('reload', function() {
@@ -40,4 +34,3 @@ gulp.task('reload', function() {
 });
 
 gulp.task('default',['webserver', 'watch']);
-// gulp.task('default',['webserver']);
---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * @author Alex Wang <allenallen2525@gmail.com>
+ */
+
+$(function () {
+    $('#menu > .container > ul > li.group > span').on('click', function () {
+        $(this).parent().toggleClass('active');
+    });
+});
