d292047479411fee067cbbf1c0e2c4e10916b005
wysalex <wysalexwang@gmail.com>
2017-05-05 17:59:31 +0800

    新增 gulp gulp-connect-php browser-sync gulp-plumber gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 var gulp           = require('gulp'),
     webserver      = require('gulp-webserver'),
-    gulpCompass    = require('gulp-compass');
+    gulpCompass    = require('gulp-compass'),
+    gulpPlumber    = require('gulp-plumber'),
+    gulpNotify     = require("gulp-notify");
+
+// var phpConnect     = require('gulp-connect-php');
+// // var browserSync    = require('browser-sync').create();
+// var browserSync    = require('browser-sync');
+
+// gulp.task('serve', function () {
+//     browserSync.init({
+//         // server: true,
+//         server: {
+//             baseDir: '/home/rd00/Git/cms',
+//             index: 'index.html',
+//         },
+//     });
+// });
+
+// gulp.task('php-server', function() {
+//     phpConnect.server({}, function (){
+//         browserSync({
+//             proxy: '127.0.0.1:8000'
+//         });
+//     });
+
+//     gulp.watch('**/*.php').on('change', function () {
+//         browserSync.reload();
+//     });
+// });
+
+// gulp.task('default', ['serve']);
+// gulp.task('php', ['php-server']);
 
 gulp.task('webserver', function() {
     gulp.src('./')
@@ -14,6 +45,7 @@ gulp.task('webserver', function() {
 
 gulp.task('compass', function () {
     gulp.src('scss/*.scss')     // sass 來源路徑
+        .pipe(gulpPlumber())
         .pipe(gulpCompass({
             css: 'css',         // compass 輸出位置
             sass: 'scss',       // sass 來源路徑
@@ -21,7 +53,8 @@ gulp.task('compass', function () {
             style: 'nested',    // CSS 處理方式，預設 nested（expanded, nested, compact, compressed）
             comments: false,    // 是否要註解，預設(true)
             require: ['susy'],  // 額外套件 susy
-        }));
+        }))
+        .pipe(gulpNotify('Compass Scss Finish'));
 });
 
 gulp.task('watch',function(){
